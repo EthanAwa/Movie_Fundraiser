5eacadfd57de02d07969424c58f17bfa84eb2a23
Ethan Sherriff <sherri.ethan@awatapu.school.nz>
2021-06-30 09:56:28 +1200

    ctf_2fort simulator

--- test_file.py
+++ test_file.py
@@ -1,3 +1,10 @@
 print("Hello, World!")
 for i in range(0, 100, 5):
     print(i)
+
+engineer = input("building: ").lower().strip()
+
+if engineer != "sentry" and engineer != "minisentry":
+    print("sap")
+else:
+    print("shoot gun")
