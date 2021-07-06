186d6912264ad7cef79a3660ca4a96d7a03b885b
Ethan Sherriff <sherri.ethan@awatapu.school.nz>
2021-07-06 12:19:28 +1200

    Remove original testing file, add comments to base file

---
+++ base_component.py
@@ -0,0 +1,28 @@
+# Imports
+
+# Functions
+
+# Main Routine
+
+# Set up dictionaries / lists
+
+# Ask user if they've used the program before
+
+# Loop code for ticket details
+
+    # Ask for name (can't be blank)
+
+    # Ask for age (12 - 130)
+
+    # Calculate ticket prices
+
+    # Loop to ask for snacks
+
+    # Calculate snack prices
+
+    # Ask for payment (apply 5% surcharge if credit card)
+
+# Calculate total ticket and snack cost
+
+# Save to a text / csv file
+
