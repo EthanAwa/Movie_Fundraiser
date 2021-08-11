6d64c9a2445fb30c073a9384b5af31bf66491216
Ethan Sherriff <sherri.ethan@awatapu.school.nz>
2021-08-12 10:20:34 +1200

    Options can now be set when calling the string_checker function

--- string_checker_V2.py
+++ string_checker_V2.py
@@ -1,28 +1,27 @@
-def yes_no_checker(question):
+def string_checker(question, options):
     valid = False
 
     while not valid:
 
-        error = "\nPlease answer with yes/no or y/n\n"
         response = input(question).lower()
 
-        if response == "yes" or response == "no":
-            return response
-        elif response == "y" or response == "nn":
-            return response
-        elif response == "quit":
+        if response in options:
             return response
         else:
-            print(error)
+            for item in options:
+                if response == item[0]:
+                    return item
 
+        print("Sorry, that isn't a valid response.")
 
 loop = True
 while loop:
-    snack_check = yes_no_checker("Do you want to buy snacks? Type \"yes\" or \"no\" (No quotes, y/n also work): ")
+    snack_check = yes_no_checker("Do you want to buy snacks? Type \"yes\" or \"no\""
+                                 "(No quotes, y/n also work): ", ["yes", "no", "quit"])
     if snack_check == "yes" or snack_check == "y":
-        print("\nOk, what would you like to order: \n")
+        print("Ok, what would you like to order: ")
     elif snack_check == "no" or snack_check == "n":
-        print("\nOk, fair enough\n")
-    else:
+        print("Ok, fair enough")
+    elif snack_check == "quit":
         loop = False
         break
